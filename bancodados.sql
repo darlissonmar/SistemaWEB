55779fe17baa9e820a16390c38df2366a5ecf900
Darlisson <ext-darlisson.jesus@nokia.com>
2012-04-10 13:29:26 -0400

    banco de dados sql versao 2

--- bancodados.sql
+++ bancodados.sql
@@ -28,46 +28,36 @@ ENGINE=InnoDB;
 
 CREATE TABLE TB_AREA (
   tb_area_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  tb_user_id INTEGER UNSIGNED NOT NULL,
+  tb_user_id INTEGER UNSIGNED,
   tb_area_nome VARCHAR(30) NOT NULL,
   PRIMARY KEY(tb_area_id),
   FOREIGN KEY(tb_user_id)
     REFERENCES TB_USUARIOS(tb_user_id)
-      ON DELETE RESTRICT
+      ON DELETE SET NULL
       ON UPDATE CASCADE
 )
 ENGINE=InnoDB;
 
 CREATE TABLE TB_DISCIPLINA (
   tb_disciplina_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  tb_user_id INTEGER UNSIGNED NOT NULL,
-  tb_area_id INTEGER UNSIGNED NOT NULL,
-  tb_disciplina_nome VARCHAR(30) NOT NULL,
+  tb_user_id INTEGER UNSIGNED,
+   tb_disciplina_nome VARCHAR(30) NOT NULL,
   PRIMARY KEY(tb_disciplina_id),
-  FOREIGN KEY(tb_area_id)
-    REFERENCES TB_AREA(tb_area_id)
-      ON DELETE CASCADE
-      ON UPDATE CASCADE,
   FOREIGN KEY(tb_user_id)
     REFERENCES TB_USUARIOS(tb_user_id)
-      ON DELETE RESTRICT
+      ON DELETE SET NULL
       ON UPDATE CASCADE
 )
 ENGINE=InnoDB;
 
 CREATE TABLE TB_ASSUNTO (
   tb_asssunto_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  tb_user_id INTEGER UNSIGNED NOT NULL,
-  tb_disciplina_id INTEGER UNSIGNED NOT NULL,
+  tb_user_id INTEGER UNSIGNED,
   tb_assunto_nome VARCHAR(30) NOT NULL,
   PRIMARY KEY(tb_asssunto_id),
-  FOREIGN KEY(tb_disciplina_id)
-    REFERENCES TB_DISCIPLINA(tb_disciplina_id)
-      ON DELETE CASCADE
-      ON UPDATE CASCADE,
   FOREIGN KEY(tb_user_id)
     REFERENCES TB_USUARIOS(tb_user_id)
-      ON DELETE RESTRICT
+      ON DELETE SET NULL
       ON UPDATE CASCADE
 );
 
@@ -75,7 +65,7 @@ CREATE TABLE TB_ASSUNTO (
 
 CREATE TABLE TB_QUESTAO (
   tb_questao_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  tb_user_id INTEGER UNSIGNED NOT NULL,
+  tb_user_id INTEGER UNSIGNED,
   tb_area_id INTEGER UNSIGNED NOT NULL,
   tb_disciplina_id INTEGER UNSIGNED NOT NULL,
   tb_questao_enunciado TEXT NOT NULL,
@@ -97,7 +87,7 @@ CREATE TABLE TB_QUESTAO (
       ON UPDATE CASCADE,
   FOREIGN KEY(tb_user_id)
     REFERENCES TB_USUARIOS(tb_user_id)
-      ON DELETE RESTRICT
+      ON DELETE SET NULL
       ON UPDATE CASCADE
 );
 
@@ -111,6 +101,33 @@ CREATE TABLE TB_ASSUNTO_has_TB_QUESTAO (
       ON UPDATE CASCADE,
   FOREIGN KEY(tb_questao_id)
     REFERENCES TB_QUESTAO(tb_questao_id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
+);
+
+CREATE TABLE TB_DISCIPLINA_has_TB_AREA (
+  tb_disciplina_id INTEGER UNSIGNED NOT NULL,
+  tb_area_id INTEGER UNSIGNED NOT NULL,
+  PRIMARY KEY(tb_disciplina_id, tb_area_id),
+  FOREIGN KEY(tb_disciplina_id)
+    REFERENCES TB_DISCIPLINA(tb_disciplina_id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE,
+  FOREIGN KEY(tb_area_id)
+    REFERENCES TB_AREA(tb_area_id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
+);
+CREATE TABLE TB_ASSUNTO_has_TB_DISCIPLINA (
+  tb_asssunto_id INTEGER UNSIGNED NOT NULL,
+  tb_disciplina_id INTEGER UNSIGNED NOT NULL,
+  PRIMARY KEY(tb_asssunto_id, tb_disciplina_id),
+  FOREIGN KEY(tb_asssunto_id)
+    REFERENCES TB_ASSUNTO(tb_asssunto_id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE,
+  FOREIGN KEY(tb_disciplina_id)
+    REFERENCES TB_DISCIPLINA(tb_disciplina_id)
       ON DELETE CASCADE
       ON UPDATE CASCADE
 );
