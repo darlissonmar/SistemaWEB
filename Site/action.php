eb8969d1add6a8581a759eb37bf10f44022378fa
Darlisson <darlisson11@yahoo.com.br>
2012-04-19 00:26:42 -0400

    modificacao action + criacao index.php e validacao.php

--- Site/action.php
+++ Site/action.php
@@ -1,35 +1,29 @@
 <?php
 
-        include("conectaBD.php");
+include("conectaBD.php");
        
-        $usuario= $_POST["login"];
-        $senha= $_POST["senha"];
+$usuario= $_POST["login"];
+$senha= $_POST["senha"];
 
-		 $busca= "select *from tb_usuarios where tb_user_email LIKE '".$usuario."' and tb_user_senha LIKE '".$senha."' ";
-		 $sql=mysql_query($busca,$conexao) or die (mysql_erro());
-		 
-		 while($linha= mysql_fetch_array($sql)){
-			 $nome=$linha['tb_user_pnome'];
-			 $tipo_de_conta=$linha['tb_user_tipo'];
-			 }
-  if (substr_count($usuario,'@')==0||substr_count($usuario,'.')==0){
-	  echo "<script>alert('Não foi possível Logar: Login e/ou Senha inválido');</script>";
-      echo "<script>location.href = 'login.php';</script>";
-	 
-	 }
-	 
-	if ($tipo_de_conta == 1){
+$busca= "select *from tb_usuarios where tb_user_login = '".$usuario."' and tb_user_senha ='".$senha."' LIMIT 1";
+$sql=mysql_query($busca,$conexao) or die (mysql_erro());
+$f = mysql_fetch_object($sql);
+
+ if (mysql_num_rows($sql)==1){
+$pnome = $f->tb_user_pnome;
+$nivel = $f->tb_user_tipo;
+$login = $f->tb_user_login;
+
+ 	if ($nivel == 1){
 	$tipo = "ADMINISTRADOR";
 	} 
- if (mysql_num_rows($sql)!=0){
-	    
-		echo "<script>alert('Bem Vindo: <$nome> : <$tipo> ');</script>";
+		echo "<script>alert('Bem Vindo: <$pnome> : <$tipo> ');</script>";
         echo "<script>location.href = 'principal.html';</script>";
-		  }
+  }
 		  
-		  else{
-			  echo "<script>alert('Não foi possível Logar: Login e/ou Senha inválido');</script>";
-              echo "<script>location.href = 'login.php';</script>";
-			  }
+else{
+ echo "<script>alert('2Não foi possível Logar: Login e/ou Senha inválido');</script>";
+ echo "<script>location.href = 'index.php';</script>";
+}
 
 ?>
